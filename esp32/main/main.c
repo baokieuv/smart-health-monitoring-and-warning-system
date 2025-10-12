edfc99e242fad3dc5bd3518a7c9baf81023ec933
baokieuv <154108649+baokieuv@users.noreply.github.com>
2025-10-12 20:29:38 +0700

    initial commit

---
+++ esp32/main/main.c
@@ -0,0 +1,168 @@
+#include <driver/gpio.h>
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_timer.h"
+#include "mqtt_client.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define TAG                 "IOT"
+#define WIFI_SSID           "LiB"
+#define WIFI_PASS           "123456789"
+#define MAXIMUM_RETRY       5
+
+#define MQTT_BROKER         "mqtt://demo.thingsboard.io:1883"
+#define MQTT_USER_NAME      "lNfdsh6tgjbgfBUC8yjn"
+
+#define RPC_REQUEST_TOPIC   "v1/devices/me/rpc/request/+"
+#define TELEMETRY_TOPIC     "v1/devices/me/telemetry"
+#define ATTRIBUTES_TOPIC    "v1/devices/me/attributes"
+
+#define WIFI_CONNECTED_BIT  BIT0
+#define WIFI_FAIL_BIT       BIT1
+#define MQTT_CONNECTED_BIT  BIT2
+
+static EventGroupHandle_t event_group;
+static int s_retry_num = 0;
+static esp_mqtt_client_handle_t client = NULL;
+
+void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(event_group, WIFI_FAIL_BIT);
+            ESP_LOGI(TAG,"connect to the AP fail");   
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(event_group, WIFI_CONNECTED_BIT);
+    }
+}
+void wifi_init(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+}
+
+void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%ld", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        xEventGroupSetBits(event_group, MQTT_CONNECTED_BIT);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        xEventGroupClearBits(event_group, MQTT_CONNECTED_BIT);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA: %s", event->topic);
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+void mqtt_init(void)
+{
+    ESP_LOGI(TAG, "STARTING MQTT");
+    esp_mqtt_client_config_t mqttConfig = {
+        .broker.address.uri = MQTT_BROKER,
+        .credentials.username = MQTT_USER_NAME,
+    };
+
+    client = esp_mqtt_client_init(&mqttConfig);
+    ESP_ERROR_CHECK(esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client));
+    ESP_ERROR_CHECK(esp_mqtt_client_start(client));
+}
+void mqtt_send_task(void *param){
+    const char *payload = "{\"cccd\": \"1234567890987\","
+                            "\"heartRate\": 70.5,"
+                            "\"O2\": 40,"
+                            "\"temperature\": 37.5,"
+                            "\"alarm\": \"normal\"}";
+
+    while(1){
+        EventBits_t bits = xEventGroupGetBits(event_group);
+        if(bits & MQTT_CONNECTED_BIT){
+            esp_mqtt_client_publish(client, TELEMETRY_TOPIC, (void*)payload, strlen(payload), 1, 0);
+            ESP_LOGI(TAG, "Telemetry sent");
+        }else{
+            ESP_LOGW(TAG, "MQTT not connected, skipping publish");
+        }
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
+
+void app_main(void){
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_2, 1);
+
+    event_group = xEventGroupCreate();
+    wifi_init();
+    
+    EventBits_t bits = xEventGroupWaitBits(event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to Wi-Fi successfully");
+        mqtt_init();
+        xTaskCreate(mqtt_send_task, "mqtt_send_task", 4096, NULL, 5, NULL);
+    } else {
+        ESP_LOGE(TAG, "Failed to connect to Wi-Fi");
+    }
+}
