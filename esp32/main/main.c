59e702a16144bde3649f746e45ed3b7ff9974cd1
baokieuv <154108649+baokieuv@users.noreply.github.com>
2025-10-19 21:57:01 +0700

    Add max30102 sensor

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -13,6 +13,9 @@
 #include "onewire_bus.h"
 #include "ds18b20.h"
 #include "cJSON.h"
+#include "max30102/max30102_api.h"
+#include "max30102/algorithm.h"
+#include "max30102/i2c_api.h"
 
 #define TAG                 "IOT"
 #define MAXIMUM_RETRY       5
@@ -31,12 +34,23 @@
 #define NVS_KEY_CCCD    "cccd"
 
 #define DS18B20_PIN     GPIO_NUM_18
+#define BUFFER_SIZE     128
+#define DELAY_AMOSTRAGEM 40
 
 static EventGroupHandle_t event_group;
 static int s_retry_num = 0;
 static esp_mqtt_client_handle_t client = NULL;
 static httpd_handle_t http_server = NULL;
 static float temperature = 0.0f;
+static int heart_rate = 0;
+static double spo2 = 0.0;
+
+int32_t red_data = 0;
+int32_t ir_data = 0;
+int32_t red_data_buffer[BUFFER_SIZE];
+int32_t ir_data_buffer[BUFFER_SIZE];
+double auto_correlationated_data[BUFFER_SIZE];
+
 
 extern const uint8_t index_html_start[] asm("_binary_index_html_start");
 extern const uint8_t index_html_end[] asm("_binary_index_html_end");
@@ -72,34 +86,6 @@ void save_wifi_config(const char *ssid, const char *pass, const char *cccd) {
     nvs_close(nvs);
     ESP_LOGI(TAG, "WiFi config saved to NVS");
 }
-
-void read_temperature(void* param){
-    onewire_bus_handle_t bus = NULL;
-    onewire_bus_config_t bus_cfg = {
-        .bus_gpio_num = DS18B20_PIN,
-        .flags = {
-            .en_pull_up = true,
-        }
-    };
-    onewire_bus_rmt_config_t rmt_cfg = {
-        .max_rx_bytes = 10,
-    };
-    ESP_ERROR_CHECK(onewire_new_bus_rmt(&bus_cfg, &rmt_cfg, &bus));
-    ESP_LOGI(TAG, "1-Wire bus installed on GPIO%d", DS18B20_PIN);
-
-    ds18b20_device_handle_t ds18b20;
-    ds18b20_config_t ds_cfg = {};
-    ESP_ERROR_CHECK(ds18b20_new_device_from_bus(bus, &ds_cfg, &ds18b20));
-
-    while(1){
-        ESP_ERROR_CHECK(ds18b20_trigger_temperature_conversion(ds18b20));
-        ESP_ERROR_CHECK(ds18b20_get_temperature(ds18b20, &temperature));
-        ESP_LOGI(TAG, "temperature read from DS18B20: %.2fC", temperature);
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
-
-}
-
 bool load_wifi_config(char *ssid, char *pass, char *cccd) {
     nvs_handle_t nvs;
     if (nvs_open(NVS_NAMESPACE, NVS_READONLY, &nvs) != ESP_OK) return false;
@@ -118,7 +104,6 @@ esp_err_t send_handler(httpd_req_t* req){
     ESP_ERROR_CHECK(httpd_resp_send(req, (char*)index_html_start, index_html_end - index_html_start));
     return ESP_OK;
 }
-
 esp_err_t receive_handler(httpd_req_t* req){
     char data[100] = { 0 };
     httpd_req_recv(req, data, req->content_len);
@@ -179,7 +164,6 @@ esp_err_t start_http_server(void){
     }
     return ESP_OK;
 }
-
 void stop_http_server(void) {
     if (http_server) {
         httpd_stop(http_server);
@@ -210,8 +194,7 @@ void start_ap_mode(void) {
     start_http_server();
     ESP_LOGI(TAG, "AP Mode started, SSID: ESP32_Config");
 }
-
-bool wifi_init_sta(const char *ssid, const char *pass)
+bool start_station_mode(const char *ssid, const char *pass)
 {
     esp_netif_create_default_wifi_sta();
 
@@ -288,6 +271,116 @@ void mqtt_init(void)
     ESP_ERROR_CHECK(esp_mqtt_client_start(client));
 }
 
+void fill_buffers_data()
+{
+	for(int i = 0; i < BUFFER_SIZE; i++){
+		read_max30102_fifo(&red_data, &ir_data);
+		ir_data_buffer[i] = ir_data;
+		red_data_buffer[i] = red_data;
+		ir_data = 0;
+		red_data = 0;
+		vTaskDelay(pdMS_TO_TICKS(DELAY_AMOSTRAGEM));
+	}
+}
+void read_max30102(void *pvParameters)
+{
+    max_config max30102_configuration = {
+
+		.INT_EN_1.A_FULL_EN         = 1,
+		.INT_EN_1.PPG_RDY_EN        = 1,
+		.INT_EN_1.ALC_OVF_EN        = 0,
+		.INT_EN_1.PROX_INT_EN       = 0,
+
+		.INT_EN_2.DIE_TEMP_RDY_EN   = 0,
+
+		.FIFO_WRITE_PTR.FIFO_WR_PTR = 0,
+
+		.OVEF_COUNTER.OVF_COUNTER   = 0,
+
+		.FIFO_READ_PTR.FIFO_RD_PTR  = 0,
+
+		.FIFO_CONF.SMP_AVE          = 0b010,  //média de 4 valores
+		.FIFO_CONF.FIFO_ROLLOVER_EN = 1,      //fifo rollover enable
+		.FIFO_CONF.FIFO_A_FULL      = 0,      //0
+
+		.MODE_CONF.SHDN             = 0,
+		.MODE_CONF.RESET            = 0,
+		.MODE_CONF.MODE             = 0b011,  //SPO2 mode
+
+		.SPO2_CONF.SPO2_ADC_RGE     = 0b01,   //16384 nA(Escala do DAC)
+		.SPO2_CONF.SPO2_SR          = 0b001,  //200 samples per second
+		.SPO2_CONF.LED_PW           = 0b10,   //pulso de 215 uS do led.
+
+		.LED1_PULSE_AMP.LED1_PA     = 0x24,   //CORRENTE DO LED1 25.4mA
+		.LED2_PULSE_AMP.LED2_PA     = 0x24,   //CORRENTE DO LED2 25.4mA
+
+		.PROX_LED_PULS_AMP.PILOT_PA = 0X7F,
+
+		.MULTI_LED_CONTROL1.SLOT2   = 0,      //Desabilitado
+		.MULTI_LED_CONTROL1.SLOT1   = 0,      //Desabilitado
+
+		.MULTI_LED_CONTROL2.SLOT4   = 0,      //Desabilitado
+		.MULTI_LED_CONTROL2.SLOT3   = 0,      //Desabilitado
+    };
+	i2c_init();
+	vTaskDelay(pdMS_TO_TICKS(100));
+	max30102_init(&max30102_configuration);
+	init_time_array();
+	uint64_t ir_mean;
+	uint64_t red_mean;
+	float temperature;
+	double r0_autocorrelation;
+
+	for(;;){
+		//vTaskDelay(pdMS_TO_TICKS(100));
+		fill_buffers_data();
+		temperature = get_max30102_temp();
+		remove_dc_part(ir_data_buffer, red_data_buffer, &ir_mean, &red_mean);
+		remove_trend_line(ir_data_buffer);
+		remove_trend_line(red_data_buffer);
+		double pearson_correlation = correlation_datay_datax(red_data_buffer, ir_data_buffer);
+		heart_rate = calculate_heart_rate(ir_data_buffer, &r0_autocorrelation, auto_correlationated_data);
+
+		printf("\n");
+		printf("HEART_RATE %d\n", heart_rate);
+		printf("correlation %f\n", pearson_correlation);
+		printf("Temperature %f\n", temperature);
+
+		if(pearson_correlation >= 0.7){ //Os dois sinais IR e RED devem ser fortemente relacionados.
+			spo2 = spo2_measurement(ir_data_buffer, red_data_buffer, ir_mean, red_mean);
+			printf("SPO2 %f\n", spo2);
+		}
+		printf("\n");
+	}
+}
+
+void read_temperature(void* param){
+    onewire_bus_handle_t bus = NULL;
+    onewire_bus_config_t bus_cfg = {
+        .bus_gpio_num = DS18B20_PIN,
+        .flags = {
+            .en_pull_up = true,
+        }
+    };
+    onewire_bus_rmt_config_t rmt_cfg = {
+        .max_rx_bytes = 10,
+    };
+    ESP_ERROR_CHECK(onewire_new_bus_rmt(&bus_cfg, &rmt_cfg, &bus));
+    ESP_LOGI(TAG, "1-Wire bus installed on GPIO%d", DS18B20_PIN);
+
+    ds18b20_device_handle_t ds18b20;
+    ds18b20_config_t ds_cfg = {};
+    ESP_ERROR_CHECK(ds18b20_new_device_from_bus(bus, &ds_cfg, &ds18b20));
+
+    while(1){
+        ESP_ERROR_CHECK(ds18b20_trigger_temperature_conversion(ds18b20));
+        ESP_ERROR_CHECK(ds18b20_get_temperature(ds18b20, &temperature));
+        ESP_LOGI(TAG, "temperature read from DS18B20: %.2fC", temperature);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+
+}
+
 void mqtt_send_task(void *param){
     char cccd[16];
     nvs_handle_t nvs;
@@ -301,14 +394,15 @@ void mqtt_send_task(void *param){
         EventBits_t bits = xEventGroupGetBits(event_group);
         if (bits & MQTT_CONNECTED_BIT) {
             snprintf(payload, sizeof(payload),
-                "{\"cccd\":\"%s\",\"heartRate\":70.5,\"O2\":97,\"temperature\":%f,\"alarm\":\"normal\"}",
-                cccd, temperature);
+                "{\"cccd\":\"%s\",\"heartRate\":%d,\"O2\":%lf,\"temperature\":%f,\"alarm\":\"normal\"}",
+                cccd, heart_rate, spo2, temperature);
             esp_mqtt_client_publish(client, TELEMETRY_TOPIC, payload, 0, 1, 0);
             ESP_LOGI(TAG, "Telemetry sent: %s", payload);
         }
         vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
+
 
 void app_main(void){
     esp_err_t ret = nvs_flash_init();
@@ -330,7 +424,7 @@ void app_main(void){
 
     if (load_wifi_config(ssid, pass, cccd)) {
         ESP_LOGI(TAG, "Loaded WiFi config: SSID=%s", ssid);
-        bool ok = wifi_init_sta(ssid, pass);
+        bool ok = start_station_mode(ssid, pass);
         if (ok) {
             stop_http_server();  
             mqtt_init();
@@ -346,4 +440,5 @@ void app_main(void){
         start_ap_mode();
     }
     xTaskCreate(read_temperature, "read temp", 4086, NULL, 5, NULL);
+    xTaskCreate(read_max30102, "read max30102", 4086, NULL, 5, NULL);
 }
